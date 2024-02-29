f21159cbd7432bcc07f8a8df2b1088f90ed93f55
Daniel Gonzalez <dgonzalez@expediagroup.com>
2024-02-28 18:58:45 -0800

    updated names

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,22 @@ def main():
 
     st.set_page_config(page_title="Little AI Bites", page_icon=None, layout="wide", initial_sidebar_state="auto")
     st.image(image_path, width=400)
-    st.write("This Streamlit app shows how we can interact with multiple AI technologies")
+
+    st.markdown("""
+    ## App Contains:
+    This streamlit demo app aims at providing various AI bot usages in one place.
+
+    # Pages:
+        1. Bot : Showcases a generic chatbot like chatgpt or gemini (on text)
+        2. BotSearch: Like the Bot example but it has access to the internet thus it is more practical.
+        3. MultiBot: Combines the power of multiple AI agents talking to each other to accomplish a task. The agents also have access to the internet.
+        4. PDF Reader: Allows you to upload multiple PDF files and converse with them. Its a great demo for researching documents.
+
+    # Next:
+        1. Will be adding more AI functionalities in the future to demostrate how we can use AI
+        2. Will continue to improve UI (Theme, Menus, etc)
+    
+    """)
     st.write('Debug - hostname',hostname)
 
 
